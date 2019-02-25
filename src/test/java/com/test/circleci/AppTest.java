ff0086887d46ded07b16db1f5bdb5ea097dc08ab
Viny Machado <vinyoliver.ti@gmail.com>
2019-02-25 17:07:08 -0400

    new test case

--- src/test/java/com/test/circleci/AppTest.java
+++ src/test/java/com/test/circleci/AppTest.java
@@ -11,4 +11,9 @@ public class AppTest {
     public void shouldBeEqual() {
         Assert.assertEquals(App.sum(10, 10), 20);
     }
+
+    @Test
+    public void shouldBeDiffent() {
+        Assert.assertNotEquals(App.sum(10, 12), 20);
+    }
 }
